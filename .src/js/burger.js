fd43e187a819c2c5a9474e2c89fe43cbaa4a76b2
Daniil <dneretin01@gmail.com>
2021-10-30 14:41:41 +0300

    ready

--- .src/js/burger.js
+++ .src/js/burger.js
@@ -1,8 +1,8 @@
 const burger = document.querySelector('.nav__burger');
-if(burger){
+if (burger) {
     const menu = document.querySelector('.nav__menu');
-    burger.addEventListener('click',function (e) {
-       burger.classList.toggle('active');
-       menu.classList.toggle('active');
+    burger.addEventListener('click', function (e) {
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
     });
 }
